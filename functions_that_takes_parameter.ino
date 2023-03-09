2cc6aff822783127e700711240180835e7945dd7
EngineerHamziey <engineerlawalhamzatademola@gmail.com>
2023-03-09 02:31:03 -0800

    push

--- functions_that_takes_parameter.ino
+++ functions_that_takes_parameter.ino
@@ -4,38 +4,47 @@
       //code to run when true
     }else if (another condition) {
       //code to run when true
+    }else if (another condition) {
+      //code to run when true
     }else {
       
     }
-  #endif
+#endif
+
+const int 
+  led1 = 2,
+  led2 = 3,
+  led3 = 4,
+  led4 = 5;
 
 //a void function is a function that doesn't return a value
-void switchLed(int ledPin, bool ledState) {
-  // if (ledState == true) {
-  //   digitalWrite(ledPin, HIGH);
+void switchLed(int ledPin, bool shouldLedOn) {
+  // if (shouldLedOn == true) {
+  //   digitalWrite(ledPin, true);
   // }else {
   //   digitalWrite(ledPin, LOW);   
   // }
   
-  digitalWrite(ledPin, ledState);   
+  // OR
+  
+  digitalWrite(ledPin, shouldLedOn);   
 }
 
 //we have 4 LEDS. We have an led one pin 3, by default
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 }
 
 
 void loop() {
   // TO Turn it on
-  switchLed(LED_BUILTIN, true);
+  switchLed(led1, true);
   delay(500);//takes in integer, and the default unit milliseconds
   
-  switchLed(LED_BUILTIN, false);
+  switchLed(led1, false);
   // TO Turn it OFF
   delay(500);
 }
